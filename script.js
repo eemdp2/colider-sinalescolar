8557e46c0211d6c3934677354bc9729a658f2087
escolamilitar <leonardo.portal@edu.mt.gov.br>
2026-03-02 17:50:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const statusTexto = document.getElementById('status');
 // Horários fixos conforme solicitado (Último às 17:10)
 const mapaHorarios = {
     "07:00": "somA1", "07:45": "somA2", "08:30": "somA3",
-    "09:15": "somA4", "09:36": "somA5", "09:40": "somA6",
-    "10:25": "somA7", "11:10": "somA8", "11:55": "somA9",
+    "09:15": "somA4", "09:28": "somA5", "09:30": "somA6",
+    "10:20": "somA7", "11:10": "somA8", "12:00": "somA9",
     "13:00": "somA1", "13:45": "somA2", "14:30": "somA3",
-    "15:15": "somA4", "15:36": "somA5", "15:40": "somA6",
-    "16:25": "somA7", "17:10": "somA9" 
+    "15:15": "somA4", "15:28": "somA5", "15:30": "somA6",
+    "16:20": "somA7", "17:10": "somA9" 
 };
 
 let sistemaAtivo = false;
